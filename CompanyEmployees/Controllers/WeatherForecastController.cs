302646bbcb8d8467e69a3f355fa79957d88ab683
whitehandedliar@gmail.com <whitehandedliar@gmail.com>
2022-11-04 16:04:35 +0300

    Add project files.

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private ILoggerManager _logger;
+        public WeatherForecastController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("info");
+            _logger.LogDebug("debug");
+            _logger.LogWarn("warn");
+            _logger.LogError("error");
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using LoggerService;
+using Microsoft.Extensions.DependencyInjection;
+using Contracts;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+    }
+}
